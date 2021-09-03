3833526fc9ba4782e92227ab1cffdb965304edd8
gabrieldvpereira <gdavi.gd@gmail.com>
2021-09-02 21:40:37 -0300

    feat: start project

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express'; 
+
+
+
+class Server { 
+    constructor(){
+        this.app = express(); 
+        this.middlewares(); 
+    }
+
+    middlewares(){
+        this.app.use('/', (req, res) => {
+            res.send("server is up!!"); 
+        })
+    }; 
+
+    routes(){
+        // call routes
+    }
+}
+
+export default new Server().app; 
