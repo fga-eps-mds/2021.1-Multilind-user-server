5413866c1f17357bdf8a5f720dad08827a65457f
gabrieldvpereira <gdavi.gd@gmail.com>
2021-10-21 23:59:49 -0300

    feat: add error log

--- src/helpers/express-adapter.js
+++ src/helpers/express-adapter.js
@@ -8,6 +8,9 @@ export const adaptRoute = (controllerMethod) => {
     if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 400) {
       res.status(httpResponse.statusCode).json(httpResponse.body);
     } else {
+      console.log(
+        `STATUS: ${httpResponse.statusCode} MESSAGE: ${httpResponse.body?.message}`
+      );
       res.status(httpResponse.statusCode).json({
         message: httpResponse.body?.message,
       });
